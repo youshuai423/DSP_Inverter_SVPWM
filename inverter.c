3cf8d94b030a5f18ebacbd9b424f5c664bc5beb1
You Shuai <648913263@qq.com>
2016-07-15 16:30:23 +0800

    修改占空比计算公式

--- inverter.c
+++ inverter.c
@@ -6,7 +6,7 @@
 #define digit 100000
 #define period 7500  // 10KHz对应时钟数，TBCLK = SYSCLKOUT
 //#define period 60000  // 10KHz对应时钟数，TBCLK = SYSCLKOUT(for test)
-#define M 0.8  // 调制度
+#define M 0.95  // 调制度
 int period_count = 0;  // 载波周期数
 int Tinv[3] = {0, 0, 0};  // 三相对应比较值
 int last[3];  // 上周期Tinv值(for test)
@@ -14,7 +14,7 @@ double Dm = 0, Dn = 0, D0 = 0;  // 占空比
 
 void ePWMSetup(void);
 double roundn(double);  // 截断小数点后位数
-interrupt void epwm1_timer_isr(void);
+interrupt void epwm4_timer_isr(void);
 
 int main()
 {
@@ -30,13 +30,13 @@ int main()
    InitPieVectTable();
 
    EALLOW;
-   PieVectTable.EPWM1_INT = &epwm1_timer_isr;  // ePWM1中断函数入口
+   PieVectTable.EPWM4_INT = &epwm4_timer_isr;  // ePWM1中断函数入口
    EDIS;
 
    ePWMSetup();
 	
    IER |= M_INT3;  // enable ePWM1 CPU_interrupt
-   PieCtrlRegs.PIEIER3.bit.INTx1 = 1;  // enable ePWM1 pie_interrupt
+   PieCtrlRegs.PIEIER3.bit.INTx4 = 1;  // enable ePWM1 pie_interrupt
 
    EINT;   // 总中断 INTM 使能
    ERTM;   // Enable Global realtime interrupt DBGM
@@ -54,123 +54,24 @@ int main()
 
 void ePWMSetup(void)
 {
-
    EALLOW;
 
-   GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1; // GPIO 初始化
-   GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO2 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO3 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO4 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO5 = 1;
-
-/*   GpioCtrlRegs.GPAMUX1.bit.GPIO6 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO7 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO8 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO9 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO10 = 1;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO11 = 1;*/
-
-   GpioCtrlRegs.GPAMUX1.bit.GPIO6 = 0;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO7 = 0;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO8 = 0;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO9 = 0;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO10 = 0;
-   GpioCtrlRegs.GPAMUX1.bit.GPIO11 = 0;
-
-   GpioCtrlRegs.GPADIR.bit.GPIO6 = 1;
-   GpioCtrlRegs.GPADIR.bit.GPIO7 = 1;
-   GpioCtrlRegs.GPADIR.bit.GPIO8 = 1;
-   GpioCtrlRegs.GPADIR.bit.GPIO9 = 1;
-   GpioCtrlRegs.GPADIR.bit.GPIO10 = 1;
-   GpioCtrlRegs.GPADIR.bit.GPIO11 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1; // GPIO 初始化为epwm输出
+	GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO2 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO3 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO4 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO5 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO6 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO7 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO8 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO9 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO10 = 1;
+	GpioCtrlRegs.GPAMUX1.bit.GPIO11 = 1;
 
    EDIS;
 
-   GpioDataRegs.GPASET.bit.GPIO6 = 1;
-   GpioDataRegs.GPASET.bit.GPIO7 = 1;
-   GpioDataRegs.GPASET.bit.GPIO8 = 1;
-   GpioDataRegs.GPASET.bit.GPIO9 = 1;
-   GpioDataRegs.GPASET.bit.GPIO10 = 1;
-   GpioDataRegs.GPASET.bit.GPIO11 = 1;
-
    // ----------------EPwm1---------------------
-   EPwm1Regs.TBPRD = period;
-   EPwm1Regs.TBPHS.half.TBPHS = 0;  // 时基周期寄存器
-   EPwm1Regs.TBCTR = 0;  // 时基计数寄存器置零
-   EPwm1Regs.TBCTL.bit.PHSDIR = TB_UP;
-   EPwm1Regs.TBCTL.bit.CLKDIV = 0;
-   EPwm1Regs.TBCTL.bit.HSPCLKDIV = 0;
-   EPwm1Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
-
-   EPwm1Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm1Regs.CMPB = 0;
-   EPwm1Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
-   EPwm1Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
-   EPwm1Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm1Regs.AQCTLA.bit.CAD = AQ_CLEAR;
-
-   EPwm1Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm1Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
-   EPwm1Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
-   EPwm1Regs.DBRED = 800; // Deadzone
-   EPwm1Regs.DBFED = 800;
-
-   EPwm1Regs.ETSEL.bit.INTEN = 1;  // 使能ePWM中断
-   EPwm1Regs.ETSEL.bit.INTSEL = ET_CTR_PRD;
-   EPwm1Regs.ETPS.all = 0x01; // interrupt on first event
-   EPwm1Regs.ETPS.bit.INTPRD = ET_1ST;
-
-   // ----------------EPwm2---------------------
-   EPwm2Regs.TBPRD = period;
-   EPwm2Regs.TBPHS.half.TBPHS = 0;  // 时基周期寄存器
-   EPwm2Regs.TBCTR = 0;  // 时基计数寄存器置零
-   EPwm2Regs.TBCTL.bit.PHSDIR = TB_UP;
-   EPwm2Regs.TBCTL.bit.CLKDIV = 0;
-   EPwm2Regs.TBCTL.bit.HSPCLKDIV = 0;
-   EPwm2Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm2Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
-
-   EPwm2Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm2Regs.CMPB = 0;
-   EPwm2Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
-   EPwm2Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
-
-   EPwm2Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm2Regs.AQCTLA.bit.CAD = AQ_CLEAR;
-
-   EPwm2Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm2Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
-   EPwm2Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
-   EPwm2Regs.DBRED = 800; // Deadzone
-   EPwm2Regs.DBFED = 800;
-
-   // ----------------EPwm3---------------------
-   EPwm3Regs.TBPRD = period;
-   EPwm3Regs.TBPHS.half.TBPHS = 0;  // 时基周期寄存器
-   EPwm3Regs.TBCTR = 0;  // 时基计数寄存器置零
-   EPwm3Regs.TBCTL.bit.PHSDIR = TB_UP;
-   EPwm3Regs.TBCTL.bit.CLKDIV = 0;
-   EPwm3Regs.TBCTL.bit.HSPCLKDIV = 0;
-   EPwm3Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm3Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
-
-   EPwm3Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm3Regs.CMPB = 0;
-   EPwm3Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
-   EPwm3Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
-
-   EPwm3Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm3Regs.AQCTLA.bit.CAD = AQ_CLEAR;
-
-   EPwm3Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm3Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
-   EPwm3Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
-   EPwm3Regs.DBRED = 800; // Deadzone
-   EPwm3Regs.DBFED = 800;
-
-   // ----------------EPwm4---------------------
    EPwm4Regs.TBPRD = period;
    EPwm4Regs.TBPHS.half.TBPHS = 0;  // 时基周期寄存器
    EPwm4Regs.TBCTR = 0;  // 时基计数寄存器置零
@@ -178,23 +79,27 @@ void ePWMSetup(void)
    EPwm4Regs.TBCTL.bit.CLKDIV = 0;
    EPwm4Regs.TBCTL.bit.HSPCLKDIV = 0;
    EPwm4Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm4Regs.TBCTL.bit.CTRMODE = TB_COUNT_UP;
+   EPwm4Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
 
    EPwm4Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm4Regs.CMPB = period / 2;
+   EPwm4Regs.CMPB = 0;
    EPwm4Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
    EPwm4Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
-
    EPwm4Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm4Regs.AQCTLA.bit.ZRO = AQ_CLEAR;
-   EPwm4Regs.AQCTLB.bit.CBU = AQ_SET;
-   EPwm4Regs.AQCTLB.bit.ZRO = AQ_CLEAR;
+   EPwm4Regs.AQCTLA.bit.CAD = AQ_CLEAR;
 
    EPwm4Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm4Regs.DBCTL.bit.POLSEL = DB_ACTV_HI;  // A不翻转，B翻转
-   EPwm4Regs.DBCTL.bit.OUT_MODE = DB_DISABLE;
+   EPwm4Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
+   EPwm4Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
+   EPwm4Regs.DBRED = 80; // Deadzone
+   EPwm4Regs.DBFED = 80;
 
-   // ----------------EPwm5---------------------
+   EPwm4Regs.ETSEL.bit.INTEN = 1;  // 使能ePWM中断
+   EPwm4Regs.ETSEL.bit.INTSEL = ET_CTR_PRD;
+   EPwm4Regs.ETPS.all = 0x01; // interrupt on first event
+   EPwm4Regs.ETPS.bit.INTPRD = ET_1ST;
+
+   // ----------------EPwm2---------------------
    EPwm5Regs.TBPRD = period;
    EPwm5Regs.TBPHS.half.TBPHS = 0;  // 时基周期寄存器
    EPwm5Regs.TBCTR = 0;  // 时基计数寄存器置零
@@ -202,21 +107,21 @@ void ePWMSetup(void)
    EPwm5Regs.TBCTL.bit.CLKDIV = 0;
    EPwm5Regs.TBCTL.bit.HSPCLKDIV = 0;
    EPwm5Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm5Regs.TBCTL.bit.CTRMODE = TB_COUNT_UP;
+   EPwm5Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
 
    EPwm5Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm5Regs.CMPB = period / 2;
+   EPwm5Regs.CMPB = 0;
    EPwm5Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
    EPwm5Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
 
    EPwm5Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm5Regs.AQCTLA.bit.ZRO = AQ_CLEAR;
-   EPwm5Regs.AQCTLB.bit.CBU = AQ_SET;
-   EPwm5Regs.AQCTLB.bit.ZRO = AQ_CLEAR;
+   EPwm5Regs.AQCTLA.bit.CAD = AQ_CLEAR;
 
    EPwm5Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm5Regs.DBCTL.bit.POLSEL = DB_ACTV_HI;  // A不翻转，B翻转
-   EPwm5Regs.DBCTL.bit.OUT_MODE = DB_DISABLE;
+   EPwm5Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
+   EPwm5Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
+   EPwm5Regs.DBRED = 80; // Deadzone
+   EPwm5Regs.DBFED = 80;
 
    // ----------------EPwm6---------------------
    EPwm6Regs.TBPRD = period;
@@ -226,25 +131,24 @@ void ePWMSetup(void)
    EPwm6Regs.TBCTL.bit.CLKDIV = 0;
    EPwm6Regs.TBCTL.bit.HSPCLKDIV = 0;
    EPwm6Regs.TBCTL.bit.SYNCOSEL = TB_CTR_ZERO;
-   EPwm6Regs.TBCTL.bit.CTRMODE = TB_COUNT_UP;
+   EPwm6Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
 
    EPwm6Regs.CMPA.half.CMPA = period / 2; // duty_cycle = 0.5
-   EPwm6Regs.CMPB = period / 2;
+   EPwm6Regs.CMPB = 0;
    EPwm6Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
    EPwm6Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO;
 
    EPwm6Regs.AQCTLA.bit.CAU = AQ_SET;
-   EPwm6Regs.AQCTLA.bit.ZRO = AQ_CLEAR;
-   EPwm6Regs.AQCTLB.bit.CBU = AQ_SET;
-   EPwm6Regs.AQCTLB.bit.ZRO = AQ_CLEAR;
+   EPwm6Regs.AQCTLA.bit.CAD = AQ_CLEAR;
 
    EPwm6Regs.DBCTL.bit.IN_MODE = DBA_ALL;
-   EPwm6Regs.DBCTL.bit.POLSEL = DB_ACTV_HI;  // A不翻转，B翻转
-   EPwm6Regs.DBCTL.bit.OUT_MODE = DB_DISABLE;
-
+   EPwm6Regs.DBCTL.bit.POLSEL = DB_ACTV_HIC;  // A不翻转，B翻转
+   EPwm6Regs.DBCTL.bit.OUT_MODE = DB_FULL_ENABLE;
+   EPwm6Regs.DBRED = 80; // Deadzone
+   EPwm6Regs.DBFED = 80;
 }
 
-interrupt void epwm1_timer_isr(void)
+interrupt void epwm4_timer_isr(void)
 {
 	double Angle = 0;
 	double theta = 0;
@@ -253,9 +157,9 @@ interrupt void epwm1_timer_isr(void)
     Angle = fmod((100 * pi * (period_count / 10000.0)),(2 * pi));
    	theta = fmod(Angle,1/3.0 * pi);
    	sector = floor( Angle / (1/3.0 * pi)) + 1;
-   	Dm = M * sin(1/3.0 * pi - theta) / 2.0;
-   	Dn = M * sin(theta) / 2.0;
-   	D0 = (0.5 - Dm - Dn) / 2.0;
+   	Dm = M * sin(1/3.0 * pi - theta);
+   	Dn = M * sin(theta);
+   	D0 = 0.5 * (1 - Dm - Dn);
    	Dm = roundn(Dm);
    	Dn = roundn(Dn);
    	D0 = roundn(D0);
@@ -305,9 +209,9 @@ interrupt void epwm1_timer_isr(void)
    		Tinv[2]++;
    	if(Tinv[2] == period)
    		Tinv[2]--;
-   	EPwm1Regs.CMPA.half.CMPA = Tinv[0];
-   	EPwm2Regs.CMPA.half.CMPA = Tinv[1];
-   	EPwm3Regs.CMPA.half.CMPA = Tinv[2];
+   	EPwm4Regs.CMPA.half.CMPA = Tinv[0];
+   	EPwm5Regs.CMPA.half.CMPA = Tinv[1];
+   	EPwm6Regs.CMPA.half.CMPA = Tinv[2];
 
     period_count++;
     if (period_count == 10000) period_count = 0;
@@ -317,7 +221,7 @@ interrupt void epwm1_timer_isr(void)
     last[2] = Tinv[2];
 
    // Clear INT flag for this timer
-    EPwm1Regs.ETCLR.bit.INT = 1;
+    EPwm4Regs.ETCLR.bit.INT = 1;
 
    // Acknowledge this interrupt to receive more interrupts from group 3
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP3;
